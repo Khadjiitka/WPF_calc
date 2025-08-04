228c34523620fa42350a9de4dc7ef24ae7dc5aa3
Viktoriia <viculia.kravtsova@gmail.com>
2025-08-04 23:54:08 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_calcul
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach(UIElement el in MainRoot.Children) // элементы сетки . чтобы получить все дочерние обьекты
+            {
+                if(el is Button)
+                {
+                    ((Button) el).Click += Button_Click;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+                textLabel.Text = "";
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(textLabel.Text, null).ToString(); //Compute посчитать матем операцию строкой
+                textLabel.Text = value;
+            }
+            else
+                textLabel.Text += str;
+
+
+        }
+    }
+}
